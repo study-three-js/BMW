acf983ac2b63c30be3d8af230038661a6b05b255
lyxlwz <1850115720@qq.com>
2023-05-23 16:55:02 +0800

    🤔 汽车和底面完成

--- src/hooks/car.ts
+++ src/hooks/car.ts
@@ -4,7 +4,28 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
 const carFun = (THREE: typeof import("three"), scene: Scene) => {
+  // 添加地面网格
+  const gridHelper = new THREE.GridHelper(10, 10)
+  // gridHelper.material.opacity = 0.2
+  // gridHelper.material.transparent = true
+  scene.add(gridHelper)
 
+  // 加载汽车
+  // 实例化gltf载入库
+  const loader = new GLTFLoader().setPath("/src/assets/model/");
+  // 实例化draco载入库
+  const dracoLoader = new DRACOLoader();
+  // 添加draco载入库
+  dracoLoader.setDecoderPath("/src/assets/draco/");
+
+  // 添加draco载入库
+  loader.setDRACOLoader(dracoLoader);
+
+  // 加载模型
+  // 这里报错把 three.js换成^0.137.5 版本
+  loader.load("bmw01.glb", gltf => {
+    scene.add(gltf.scene)
+  })
 }
 
 export default carFun
