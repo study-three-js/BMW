9467380cb522af57d408baaf0f297a1234ffb66d
lyxlwz <1850115720@qq.com>
2023-05-23 17:15:20 +0800

    添加灯光

--- src/hooks/car.ts
+++ src/hooks/car.ts
@@ -11,20 +11,21 @@ const carFun = (THREE: typeof import("three"), scene: Scene) => {
   scene.add(gridHelper)
 
   // 加载汽车
-  // 实例化gltf载入库
-  const loader = new GLTFLoader().setPath("/src/assets/model/");
   // 实例化draco载入库
   const dracoLoader = new DRACOLoader();
   // 添加draco载入库
   dracoLoader.setDecoderPath("/src/assets/draco/");
 
+  // 实例化gltf载入库
+  const loader = new GLTFLoader().setPath("/src/assets/model/");
   // 添加draco载入库
   loader.setDRACOLoader(dracoLoader);
 
   // 加载模型
   // 这里报错把 three.js换成^0.137.5 版本
   loader.load("bmw01.glb", gltf => {
-    scene.add(gltf.scene)
+    const bmw = gltf.scene
+    scene.add(bmw)
   })
 }
 
--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -33,6 +33,46 @@ const initFun = (THREE: typeof import("three")) => {
   });
 
   const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+
+  // 设置z轴第一盏灯光
+  const light1 = new THREE.DirectionalLight(0xffffff, 1);
+  light1.position.set(0, 0, 10);
+  scene.add(light1);
+  // 设置z轴第二盏灯光
+  const light2 = new THREE.DirectionalLight(0xffffff, 1);
+  light2.position.set(0, 0, -10);
+  scene.add(light2);
+
+  // 设置x轴第三盏灯光
+  const light3 = new THREE.DirectionalLight(0xffffff, 1);
+  light3.position.set(10, 0, 0);
+  scene.add(light3);
+  // 设置x轴第四盏灯光
+  const light4 = new THREE.DirectionalLight(0xffffff, 1);
+  light4.position.set(-10, 0, 0);
+  scene.add(light4);
+
+  // 设置y轴第五盏灯光
+  const light5 = new THREE.DirectionalLight(0xffffff, 1);
+  light5.position.set(0, 10, 0);
+  scene.add(light5);
+  // 设置y轴第六盏灯光
+  const light6 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light6.position.set(5, 10, 0);
+  scene.add(light6);
+
+  const light7 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light7.position.set(0, 10, 5);
+  scene.add(light7);
+
+  const light8 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light8.position.set(0, 10, -5);
+  scene.add(light8);
+
+  const light9 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light9.position.set(-5, 10, 0);
+  scene.add(light9);
 
   return {
     scene,
